3b5e503e2324ef28c3810c9433d9424bd90c595e
ziggeek <i@zig-dev.ru>
2023-11-29 11:21:19 +0400

    RTKIT - Создание структурф проекта

---
+++ printed-form/src/main/java/com/ebito/orchestrator/PrintedFormApplication.java
@@ -0,0 +1,13 @@
+package com.ebito.orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintedFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrintedFormApplication.class, args);
+    }
+
+}
