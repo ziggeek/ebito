abb1c22b0ff55c90e31d4ccf3e6ef4000d17b3bf
ziggeek <i@zig-dev.ru>
2023-12-04 01:24:12 +0400

    RTKIT - Добавлен базовый request и response

--- orchestrator/src/main/java/com/ebito/orchestrator/model/request/ReferenceGenerationRequest.java
+++ orchestrator/src/main/java/com/ebito/orchestrator/model/request/ReferenceGenerationRequest.java
@@ -5,9 +5,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Value;
 
+/**
+ * Запрос для генерации новой справки
+ */
 @Value
 @Builder
-public class FormGenerationRequest {
+public class ReferenceGenerationRequest {
 
     @Schema(description = "Код справки",
             example = "001")
--- orchestrator/src/main/java/com/ebito/orchestrator/model/response/ReferenceGenerationResponse.java
+++ orchestrator/src/main/java/com/ebito/orchestrator/model/response/ReferenceGenerationResponse.java
@@ -4,21 +4,24 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Value;
 
+/**
+ * Ответ сгенерированной справки
+ */
 @Value
 @Builder
-public class FormGenerationResponse {
+public class ReferenceGenerationResponse {
 
 
     @Schema(description = "Ссылка на документ",
-            example = "/api/v1/forms/")
+            example = "/api/v1/forms/001_created_01_01_1970_08_40_12.pdf")
     String link;
 
-    @Schema(description = "Имя документа",
+    @Schema(description = "Тип справки",
             example = "Выписка по начислениям абонента")
     String name;
 
-    @Schema(description = "Имя документа",
-            example = "created_01_01_1970_08_40_12.pdf")
+    @Schema(description = "Имя справки",
+            example = "001_created_01_01_1970_08_40_12.pdf")
     String pdfFileName;
 
 }
