681b5d9e9e95ee3c92a1d7cb1abb61d079f04057
Ilyas <xxxilyas92xxx@gmail.com>
2023-12-10 13:40:03 +0500

    Минимально рабочий проект

--- cloud/src/main/java/com/ebito/cloud/api/CloudApi.java
+++ cloud/src/main/java/com/ebito/cloud/api/CloudApi.java
@@ -2,6 +2,7 @@ package com.ebito.cloud.api;
 
 import com.ebito.cloud.model.response.PrintedGuids;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,13 +15,12 @@ public interface CloudApi {
 
     @GetMapping("/get-reference/{link}")
     //todo должен вернуть файл (этот метод только фронт вызывает, оркестратору не нужен этот метод)
-    ResponseEntity<MultipartFile> getReferenceByLink(@PathVariable("link") String link);
-
+    ResponseEntity<Resource> getReferenceByLink(@PathVariable("link") String link);
+    // Комент от Артура: я буду тебе передавать в названии файла текст с именем файла для поля PrintedGuids.name
+    // - разберешься как из MultipartFile извлекать
     @PostMapping("/{clientId}/save-client-reference")
-        //todo: сделать описание подробное
-    // Комент от Артура: я буду тебе передавать в названии файла текст с именем файла для поля PrintedGuids.name - разберешься как из MultipartFile извлекать
     ResponseEntity<PrintedGuids> saveClientReference(@PathVariable("clientId") String clientId,
-                                                     @RequestPart(name = "file") MultipartFile file);
+                                                     @RequestPart(name = "file")MultipartFile file);
 
     @GetMapping("/{clientId}/get-client-references")
     ResponseEntity<List<PrintedGuids>> getClientReferences(@PathVariable("clientId") String clientId);
