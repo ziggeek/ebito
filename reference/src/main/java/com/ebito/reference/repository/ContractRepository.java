81e35f6fdeda5551349160876ed070c388b20a05
Renat Skosarev <skosarrrev@gmail.com>
2023-12-15 22:58:10 +0500

    Add JPA repositories for models

---
+++ reference/src/main/java/com/ebito/reference/repository/ContractRepository.java
@@ -0,0 +1,7 @@
+package com.ebito.reference.repository;
+
+import com.ebito.reference.model.Contract;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContractRepository extends JpaRepository<Contract, Integer> {
+}
