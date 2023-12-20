3d3cd2b18034e2c9a85a5fa66d60beb9a3dbe1fb
ziggeek <i@zig-dev.ru>
2023-12-20 23:08:09 +0400

    RTKIT - fix

--- printed-form/src/main/java/com/ebito/printed_form/api/PrinterApi.java
+++ printed-form/src/main/java/com/ebito/printed_form/api/PrinterApi.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Tag(name = "Reference", description = "Reference API")
+@Tag(name = "PrintedForm", description = "PrintedForm API")
 @RequestMapping("/api/v1")
 public interface PrinterApi {
 
