6385bfc41b08e1a300b5fcfa4d6e2e86a187245a
Renat Skosarev <skosarrrev@gmail.com>
2024-01-04 16:21:43 +0500

    Описание API для Swagger

--- reference/src/main/java/com/ebito/reference/api/ReferenceApi.java
+++ reference/src/main/java/com/ebito/reference/api/ReferenceApi.java
@@ -2,6 +2,9 @@ package com.ebito.reference.api;
 
 import com.ebito.reference.model.request.ReferenceGenerationRequest;
 import com.ebito.reference.model.response.PrintedGuids;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Tag(name = "Reference", description = "Reference API")
 @RequestMapping("/api/v1")
 public interface ReferenceApi {
-
+    @Operation(summary = "Сгенерировать форму определенного типа для клиента")
     @PostMapping("/{clientId}/generate-reference")
-        //todo: сделать описание подробное
     ResponseEntity<PrintedGuids> generateReference(@PathVariable("clientId") long clientId,
                                                    @RequestBody ReferenceGenerationRequest request);
 }
