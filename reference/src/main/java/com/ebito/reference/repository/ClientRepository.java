1170c7893f3a00114cb3f5edd2e02157ac30ba55
Renat Skosarev <skosarrrev@gmail.com>
2024-01-04 17:53:30 +0500

    Обновление типа первичного ключа в JPA-репозиториях
    Тип первичных ключей в БД и моделях был изменен, теперь вношу изменения в JPA-репозитории

--- reference/src/main/java/com/ebito/reference/repository/ClientRepository.java
+++ reference/src/main/java/com/ebito/reference/repository/ClientRepository.java
@@ -3,5 +3,5 @@ package com.ebito.reference.repository;
 import com.ebito.reference.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientRepository extends JpaRepository<Client, Integer> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
