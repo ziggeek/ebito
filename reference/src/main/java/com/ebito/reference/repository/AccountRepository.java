08d0216a8e6bf508cf25d8de1c2ccf34d64a8bec
Renat Skosarev <skosarrrev@gmail.com>
2024-01-04 22:15:20 +0500

    Удаление неиспользуемого кода, оптимизация импортов

--- reference/src/main/java/com/ebito/reference/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ebito.reference.repository;
-
-import com.ebito.reference.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
--- reference/src/main/java/com/ebito/reference/repository/ContractRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ebito.reference.repository;
-
-import com.ebito.reference.model.Contract;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContractRepository extends JpaRepository<Contract, Long> {
-}
