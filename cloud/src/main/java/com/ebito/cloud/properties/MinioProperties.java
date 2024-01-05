39bb413e57e873d9206ab9dce3b458cf5edc8bf4
Ilyas <xxxilyas92xxx@gmail.com>
2024-01-05 12:06:46 +0500

    Настройка запуска minio

--- cloud/src/main/java/com/ebito/cloud/properties/MinioProperties.java
+++ cloud/src/main/java/com/ebito/cloud/properties/MinioProperties.java
@@ -2,7 +2,6 @@ package com.ebito.cloud.properties;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.stereotype.Component;
 
