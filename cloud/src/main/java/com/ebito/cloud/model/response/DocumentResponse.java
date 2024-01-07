d8519cb014f03be532da773224d3cf9d6ecf96a5
Ilyas <xxxilyas92xxx@gmail.com>
2024-01-07 15:07:34 +0500

    Изменил PrintedGuids на DocumentResponse

---
+++ cloud/src/main/java/com/ebito/cloud/model/response/DocumentResponse.java
@@ -0,0 +1,21 @@
+package com.ebito.cloud.model.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Value;
+
+/**
+ * Ответ сгенерированной справки
+ */
+@Value
+@Builder
+public class DocumentResponse {
+
+
+    @Schema(description = "Ссылка на документ",
+            example = "https://example.com/12345.pdf")
+    String link;
+
+
+
+}
