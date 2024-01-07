5279223657796466be4c934ff7c7e97b0e956b06
Ilyas <xxxilyas92xxx@gmail.com>
2024-01-07 15:10:10 +0500

    Мелкие исправления для красоты

--- cloud/src/main/java/com/ebito/cloud/properties/MinioProperties.java
+++ cloud/src/main/java/com/ebito/cloud/properties/MinioProperties.java
@@ -1,22 +1,16 @@
 package com.ebito.cloud.properties;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
 @Data
-//@ConfigurationProperties(prefix = "minio")
-@ConstructorBinding
+@Component
+@ConfigurationProperties(prefix = "minio")
 public class MinioProperties {
-    @Value("${spring.minio.bucket}")
     private String bucket;
-    @Value("${spring.minio.url}")
     private String url;
-    @Value("${spring.minio.accessKey}")
     private String accessKey;
-    @Value("${spring.minio.secretKey}")
     private String secretKey;
 
 }
